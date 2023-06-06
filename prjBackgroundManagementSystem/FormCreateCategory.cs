ab3bea4f38db53e909d5a469476cc5b2caa0fec1
xiaowen1231 <shiao1124@gmail.com>
2023-06-06 09:21:21 +0800

    Revert "Revert "Merge branch 'jimmy'""
    This reverts commit 2ce438602b44e8d4e95b4cc747e0461e4f246053.

---
+++ prjBackgroundManagementSystem/FormCreateCategory.cs
@@ -0,0 +1,42 @@
+﻿using Onique.EStore.SqlDataLayer.EFModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjBackgroundManagementSystem
+{
+    public partial class FormCreateCategory : Form
+    {
+        public FormCreateCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComfirmAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtCategoryName.Text;
+            bool isInt = int.TryParse(txtDisplayOrder.Text, out int DisplatOrder);
+
+            var db = new AppDbContext();
+            var category = new Category()
+            {
+                CategoryName = name,
+                DisplayOrder = DisplatOrder
+            };
+            db.Categories.Add(category);
+            db.SaveChanges();
+
+        }
+
+        private void btnCancelAdd_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
