60e1ea0865a86b51d36df18b1e2d9f9bb4f0cc28
xiaowen1231 <shiao1124@gmail.com>
2023-06-06 12:40:31 +0800

    1240

---
+++ Onique.EStore.SqlDataLayer/Dto/Assembler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onique.EStore.SqlDataLayer
+{
+    public static class Assembler
+    {
+        public static Func<SqlDataReader, CategoryDto> CategoryDtoAssembler
+        {
+            get
+            {
+                Func<SqlDataReader, CategoryDto> func = (reader) =>
+                {
+                    int id = int.Parse(reader["CategoryId"].ToString());
+                    string name = reader["CategoryName"].ToString();
+                    int displayOrder = int.Parse(reader["DisplayOrder"].ToString());
+
+                    return new CategoryDto
+                    {
+                        CategoryId = id,
+                        CategoryName = name,
+                        DisplayOrder = displayOrder,
+                    };
+                };
+                return func;
+            }
+        }
+    }
+}
