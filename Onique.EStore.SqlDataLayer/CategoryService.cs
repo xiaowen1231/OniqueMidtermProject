c755b4217883b0c603461b90ec3f10312a83b75a
Jimmychang06 <129496213+Jimmychang06@users.noreply.github.com>
2023-06-06 08:36:05 +0800

    1

---
+++ Onique.EStore.SqlDataLayer/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onique.EStore.SqlDataLayer
+{
+    public class CategoryService
+    {
+        public int Update(CategoryDto dto)
+        {
+            var repo = new CategoryRepository();
+            var dtoInDb = repo.GetByName(dto.CategoryName);
+            if (dtoInDb != null && dtoInDb.CategoryId != dto.CategoryId)
+            {
+                throw new Exception("抱歉，此分類名稱已存在，無法更新");
+            }
+
+            int rows = repo.Update(dto);
+            return rows;
+        }
+    }
+}
--- prjBackgroundManagementSystem/FormCategory.cs
+++ prjBackgroundManagementSystem/FormCategory.cs
@@ -1,4 +1,5 @@
-﻿using Onique.EStore.SqlDataLayer.EFModels;
+﻿using Onique.EStore.SqlDataLayer;
+using Onique.EStore.SqlDataLayer.EFModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,33 +12,79 @@ using System.Windows.Forms;
 
 namespace prjBackgroundManagementSystem
 {
-    public partial class FormCategory : Form
+    public partial class FormCategory : Form,IGrid
     {
-        
+        List<CategoryDto> categories;
         public FormCategory()
         {
             InitializeComponent();
         }
 
-        private void Display()
+        public void Display()
         {
+            cboxCategoryName.Items.Clear();
+            cboxCategoryName.Items.AddRange((new AppDbContext().Categories.Select(c => c.CategoryName)).ToArray());
+            int? categoryId = null;
+            if (int.TryParse(this.txtCategoryId.Text, out int number))
+            {
+                categoryId = number;
+            }
+
+            string name = cboxCategoryName.Text;
+
             var db = new AppDbContext();
-            var categories = from c in db.Categories.AsNoTracking()
-                             join p in db.Products.AsNoTracking()
-                             on c.CategoryId equals p.ProductCategoryId
-                             select new
-                             {
-                                 CategoryId = c.CategoryId,
-                                 CategoryName = c.CategoryName,
-                                 ProductName = p.ProductName
-                             };
-        
-            this.dataGridView1.DataSource = categories.ToList();
+            var query = db.Categories.Select(x => new CategoryDto
+            {
+                CategoryId = x.CategoryId,
+                CategoryName = x.CategoryName,
+                DisplayOrder = x.DisplayOrder,
+            }
+            );
+          
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(c => c.CategoryId == categoryId.Value);
+            }
+            if (!string.IsNullOrEmpty(name) )
+            {
+                query = query.Where(c => c.CategoryName.Contains(name));
+            }
+            categories = query.ToList();
+
+
+            this.dataGridView1.DataSource = categories;
+
         }
 
         private void FormCategory_Load(object sender, EventArgs e)
         {
+            
             Display();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Display();           
+        }
+
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = new List<CategoryDto>();
+        }
+
+        private void btnAddCategories_Click(object sender, EventArgs e)
+        {
+            var createCategory = new FormCreateCategory();
+            createCategory.ShowDialog();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            int categoryId = categories[e.RowIndex].CategoryId;
+            var updateOrDelete = new FormEditCategory(categoryId);
+            updateOrDelete.ShowDialog();
         }
     }
 }
